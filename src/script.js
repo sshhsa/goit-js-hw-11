4ee5e2ce3888fff1a892078dad5c17af89b57b43
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-29 01:06:23 +0300

    Commit

--- src/script.js
+++ src/script.js
@@ -3,37 +3,6 @@ const input = document.querySelector('.js-input');
 const button = document.querySelector('.js-button');
 const listGallery = document.querySelector('.gallery');
 
-// function createMarkup(array) {
-//   return array
-//     .map(
-//       ({
-//         hits: [
-//           {
-//             tags,
-//             webformatURL,
-//             largeImageURL,
-//             views,
-//             downloads,
-//             likes,
-//             comments,
-//           },
-//         ],
-//       }) => `<div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">${likes}<b>Likes</b></p>
-//     <p class="info-item">${views}<b>Views</b></p>
-//     <p class="info-item">${comments}<b>Comments</b></p>
-//     <p class="info-item">${downloads}<b>Downloads</b></p>
-//   </div>
-// </div>
-// `
-//     )
-//     .join('');
-// }
-
-////////////////////////////////////////////////////////////////////////////////////////
-
 form.addEventListener('submit', onHandlerClickButton);
 
 function onHandlerClickButton(event) {
@@ -41,19 +10,28 @@ function onHandlerClickButton(event) {
   const { searchQuery } = event.currentTarget.elements;
 
   getGallery(searchQuery.value)
-    .then(data => console.log(typeof data))
+    .then(data => (listGallery.innerHTML = createMarkup(data.hits)))
+    .then(data =>
+      listGallery.insertAdjacentHTML(
+        'afterend',
+        '<div class="container-button"><button class="load-more">LOAD MORE</button></div>'
+      )
+    )
     .catch(error => console.log(error));
 }
 
 function getGallery(q) {
-  const BASE_URL = 'https://pixabay.com/api';
+  const BASE_URL = 'https://pixabay.com/api/';
   const API_KEY = '35838965-00a6ae99c457ac18fcac9dde6';
 
   return fetch(
-    `${BASE_URL}/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`
+    `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
+      console.log(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
     }
     return response.json();
   });
@@ -63,24 +41,19 @@ function createMarkup(array) {
   return array
     .map(
       ({
-        hits: [
-          {
-            tags,
-            webformatURL,
-            largeImageURL,
-            views,
-            downloads,
-            likes,
-            comments,
-          },
-        ],
+        tags,
+        webformatURL,
+        views,
+        downloads,
+        likes,
+        comments,
       }) => `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height="200" class="card-photo"/>
   <div class="info">
-    <p class="info-item">${likes}<b>Likes</b></p>
-    <p class="info-item">${views}<b>Views</b></p>
-    <p class="info-item">${comments}<b>Comments</b></p>
-    <p class="info-item">${downloads}<b>Downloads</b></p>
+    <p class="info-item"><b>Likes</b>${likes}</p>
+    <p class="info-item"><b>Views</b>${views}</p>
+    <p class="info-item"><b>Comments</b>${comments}</p>
+    <p class="info-item"><b>Downloads</b>${downloads}</p>
   </div>
 </div>
 `
