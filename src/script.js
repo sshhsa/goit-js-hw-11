660944809a2129d4a518295f306d4ab1866b1b73
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-28 22:55:07 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,27 +1,60 @@
 const form = document.getElementById('search-form');
 const input = document.querySelector('.js-input');
 const button = document.querySelector('.js-button');
+const listGallery = document.querySelector('.gallery');
 
-form.addEventListener('click', onHandlerClickButton);
+// function createMarkup(array) {
+//   return array
+//     .map(
+//       ({
+//         hits: [
+//           {
+//             tags,
+//             webformatURL,
+//             largeImageURL,
+//             views,
+//             downloads,
+//             likes,
+//             comments,
+//           },
+//         ],
+//       }) => `<div class="photo-card">
+//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">${likes}<b>Likes</b></p>
+//     <p class="info-item">${views}<b>Views</b></p>
+//     <p class="info-item">${comments}<b>Comments</b></p>
+//     <p class="info-item">${downloads}<b>Downloads</b></p>
+//   </div>
+// </div>
+// `
+//     )
+//     .join('');
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+form.addEventListener('submit', onHandlerClickButton);
 
 function onHandlerClickButton(event) {
   event.preventDefault();
+  const { searchQuery } = event.currentTarget.elements;
 
-  //   const { q } = event.currentTarget.elements;
-  //   getGallery(q.value);
-
-  createMarkup();
+  getGallery(searchQuery.value)
+    .then(data => console.log(typeof data))
+    .catch(error => console.log(error));
 }
 
 function getGallery(q) {
-  const BASE_URL = 'https://pixabay.com/api/';
+  const BASE_URL = 'https://pixabay.com/api';
   const API_KEY = '35838965-00a6ae99c457ac18fcac9dde6';
 
-  return fetch(`${BASE_URL}?key=${API_KEY}&q=${q}`).then(response => {
+  return fetch(
+    `${BASE_URL}/?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
-
     return response.json();
   });
 }
