ca2d73ee8937d701532d656d51d7b66279e0c29b
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-04-30 14:08:47 +0300

    Commit

--- src/script.js
+++ src/script.js
@@ -1,58 +1,88 @@
 import Notiflix, { Notify } from 'notiflix';
 
 const form = document.getElementById('search-form');
-// const input = document.querySelector('.js-input');
-// const button = document.querySelector('.js-button');
 const listGallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.js-load');
+const buttonSubmit = document.querySelector('.button-submit');
+const input = document.querySelector('.input-form');
+let currentPage = 1;
+
+// --------- INPUT ---------
+
+input.addEventListener('input', onInputValue);
+
+function onInputValue() {
+  if (input.value.trim() !== '') {
+    buttonSubmit.removeAttribute('disabled');
+  } else {
+    buttonSubmit.setAttribute('disabled', true);
+  }
+}
+
+// --------- ADDITION CHECKING ---------
+
+// --------- BUTTON LOAD MORE ---------
+
+loadMore.addEventListener('click', onClickButtonToLoad);
+
+function onClickButtonToLoad() {
+  currentPage += 1;
+  getGallery(form.searchQuery.value, currentPage)
+    .then(data => {
+      listGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      if (currentPage === data.totalHits) {
+        loadMore.hidden = true;
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+// --------- FORM ---------
 
 form.addEventListener('submit', onHandlerClickButton);
 
 function onHandlerClickButton(event) {
   event.preventDefault();
   const { searchQuery } = event.currentTarget.elements;
+  currentPage = 1;
 
-  getGallery(searchQuery.value)
-    .then(data => (listGallery.innerHTML = createMarkup(data.hits)))
-    .then(data =>
-      listGallery.insertAdjacentHTML(
-        'afterend',
-        '<div class="container-button"><button class="load-more">Load more</button></div>'
-      )
-    )
+  getGallery(searchQuery.value, currentPage)
+    .then(data => {
+      listGallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      if (currentPage !== data.totalHits) {
+        loadMore.hidden = false;
+      }
+    })
     .catch(error => console.log(error));
 }
+
+// --------- BACKEND ---------
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '35838965-00a6ae99c457ac18fcac9dde6';
 
-function getGallery(q) {
+function getGallery(q, page = 1) {
   const params = new URLSearchParams({
+    q: q,
     limit: 20,
-    page: 1,
+    page,
+    safesearch: true,
+    orientation: 'horizontal',
+    image_type: 'photo',
   });
 
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: API_KEY,
-    },
-  };
-
-  return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${q}&image_type=photo&orientation=horizontal&safesearch=true${params}`,
-    options
-  ).then(response => {
+  return fetch(`${BASE_URL}?key=${API_KEY}&${params}`).then(response => {
     if (!response.ok) {
-      throw new Error(
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        ),
-        response.statusText
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
       );
+      throw new Error(response.statusText);
     }
     return response.json();
   });
 }
+
+// --------- A CREATING THE MARKUP ---------
 
 function createMarkup(array) {
   return array
